67b43ac96bafcf49bfea885e293b84f4b49d255b
RAHUL KUMAR PANDEY <51964644+Rahulrkp1@users.noreply.github.com>
2022-03-27 14:40:22 +0530

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -12,6 +12,7 @@ st.text("""
 Using Machine learning algorithm , we detects if someone have diabetes or not .
 
 Diabetes Mellitus is among critical diseases and lots of people are suffering from this disease.
+
 Age, obesity, lack of exercise, hereditary diabetes, living style, bad diet, high blood pressure, etc. can cause Diabetes Mellitus. 
 People having diabetes have high risk of diseases like heart disease, kidney disease, stroke, eye problem, nerve damage, etc. 
 Current practice in hospital is to collect required information for diabetes diagnosis through various tests and appropriate treatment is provided based on diagnosis. 
@@ -20,7 +21,7 @@ Healthcare industries have large volume databases.
 Using big data analytics one can study huge datasets and find hidden information, hidden patterns to discover knowledge from the data and predict outcomes accordingly. 
 In existing method, the classification and prediction accuracy is not so high. 
 In this paper, we have proposed a diabetes prediction model for better classification of diabetes which includes few external factors responsible for diabetes along with regular factors like Glucose, BMI, Age, Insulin, etc.
-""",text-alignment='center')
+""")
 
 #open and display an image
 image = Image.open('da.jpg')
